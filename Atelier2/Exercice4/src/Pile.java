0ef920fcbec168ed8e5cdfd5eb0ffb18cdef882a
Mohamed-Yuta <allali.mohamedamine22@gmail.com>
2024-03-08 00:41:08 +0100

    Exercice4

--- Atelier2/Exercice4/src/Pile.java
+++ Atelier2/Exercice4/src/Pile.java
@@ -37,26 +37,5 @@ public class Pile {
         }
     }
 
-    // Classe interne représentant un élément de pile
-    private static class ElementPile {
-        private int valeur;
-        private ElementPile suivant;
 
-        public ElementPile(int valeur) {
-            this.valeur = valeur;
-            this.suivant = null;
-        }
-
-        public int getValeur() {
-            return valeur;
-        }
-
-        public ElementPile getSuivant() {
-            return suivant;
-        }
-
-        public void setSuivant(ElementPile suivant) {
-            this.suivant = suivant;
-        }
-    }
 }
